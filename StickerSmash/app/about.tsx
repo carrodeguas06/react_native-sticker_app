fb482818b4699d80aaed7a1fe11e860d5225a625
Bruno Carrodeguas <brunocarrodeguas@gmail.com>
2025-09-15 13:48:18 +0200

    Añadir ruta about

--- StickerSmash/app/about.tsx
+++ StickerSmash/app/about.tsx
@@ -1,8 +1,9 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import Index from '.';
 
-const Index = () =>(
+const AboutScreen = () => (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 
@@ -11,12 +12,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     color: '#fff',
   },
 });
 
-export default Index;
+export default AboutScreen;
