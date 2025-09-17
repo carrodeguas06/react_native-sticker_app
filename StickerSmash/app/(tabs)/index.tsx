593b4498ce521ff08026954934c582432a681d7a
Bruno Carrodeguas <brunocarrodeguas@gmail.com>
2025-09-17 11:58:41 +0200

    Crear componente ImageViwer

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import { Text, View,  StyleSheet } from 'react-native';
 
-import { Image } from 'expo-image';
+import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 const Index = () =>(
     <View style={styles.container}>
      <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
       </View>
     </View>
   );
@@ -22,13 +22,6 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
 });
-
-
 
 export default Index;
